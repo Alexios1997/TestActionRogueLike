8ff1ce4597f0870c66c46ce825ee325d25f3c45a
AlexB <boudis979711@gmail.com>
2025-12-04 04:32:48 +0200

    First Setup

---
+++ Source/TestActionRogueLike/TestActionRogueLike.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TestActionRogueLike.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TestActionRogueLike, "TestActionRogueLike" );
